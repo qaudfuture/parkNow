4e56cdf6e27552e6ff8e1ee5735fa1b44032b7a2
quadfuture.in@gmail.com <harshavardhan.bellipady@lisec.com>
2023-07-16 21:41:57 +0400

    husky hook setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community',
+  extends: [
+    '@react-native-community',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:prettier/recommended'],
+    'plugin:prettier/recommended',
+  ],
   plugins: ['prettier', '@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   rules: {
     'react/jsx-user-react': 1,
-    'arrow-body-style': ['error']
-  }
+    'arrow-body-style': ['error'],
+  },
 };
